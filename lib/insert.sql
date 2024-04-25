1814831b8412e3eba1a692a77169eeee7602102b
Sylviachelimo <chelimosylvia280@gmail.com>
2024-04-25 15:40:35 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+-- Insert bears into the bears table
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 3, 'M', 'Black', 'Energetic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 4, 'F', 'Brown', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 6, 'M', 'Brown', 'Disciplined', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 2, 'F', 'White', 'Playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 7, 'M', 'Green', 'Grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 8, 'F', 'Brown', 'Sweet', 1);
+
+INSERT INTO bears (age, sex, color, temperament, alive)
+VALUES (1, 'M', 'Brown', 'Adventurous', 1);
